88f39945e66a56168aca6ba0f89ccef9ca8fccfc
Bima zainudin ikhsan <35645312+bimaikhsan@users.noreply.github.com>
2021-01-10 21:11:24 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
 <?php 
 
-$koneksi = mysqli_connect("localhost","root","","sistem-evoting");
+$koneksi = mysqli_connect("sql12.freemysqlhosting.net","sql12386136","FfFePD7U8D","sql12386136");
 
 if (mysqli_connect_error()){
 	echo "koneksi database gagal" .mysqli_connect_error();
